9faf5153c803e3befd199d73a7840b03abe326f1
Louis Dalligos <zindex14@gmail.com>
2019-09-28 13:30:43 +0800

    Add updated and delete route api

--- routes/therapy.route.js
+++ routes/therapy.route.js
@@ -63,8 +63,61 @@ router.post(
 // @route    PUT api/therapies/:id
 // @desc     Update/Edit therapy article
 // @access   Private
-router.put("/", (req, res) => {
-    res.send('Edit article');
+router.put("/:id", auth, async (req, res) => {
+    const { title, description, status } = req.body;
+
+    // Build therapy object
+    const theraphyFields = {};
+
+    if (title) theraphyFields.title = title;
+    if (description) theraphyFields.description = description;
+    if (status) theraphyFields.status = status;
+
+    try {
+        let theraphy = await Therapy.findById(req.params.id);
+
+        if (!theraphy) return res.status(404).json({ msg: 'Therapy not found'});
+
+        // make sure user owns the article
+        if (theraphy.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'You are not authorized to perform this action'});
+        }
+
+        // update logic
+        theraphy = await Therapy.findByIdAndUpdate(
+            req.params.id, 
+            { $set: theraphyFields },
+            { new: true }
+        );
+
+        res.json(theraphy);
+    } catch (err) {
+        console.error(err.message)
+    }
+});
+
+
+// @route    DELETE api/therapies/:id
+// @desc     Delete therapy article
+// @access   Private
+router.delete("/:id", auth, async (req, res) => {
+
+
+    try {
+        let theraphy = await Therapy.findById(req.params.id);
+
+        if (!theraphy) return res.status(404).json({ msg: 'Therapy not found'});
+
+        // make sure user owns the article
+        if (theraphy.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'You are not authorized to perform this action'});
+        }
+
+        await Therapy.findByIdAndRemove(req.params.id);
+        res.json({ msg: 'Article has been removed' });
+    } catch (err) {
+        console.error(err.message)
+    }
 });
 
 
